d18d0b083a0491e216ce57bc0a90f56382e15b4e
Joseph El Khoury <joseph.elkhoury1@gmail.com>
2012-11-07 16:11:57 +0100

    search filters for pagination

--- main/models/mongooseModel.js
+++ main/models/mongooseModel.js
@@ -322,8 +322,8 @@ Place.statics.findAll = function (callback) {
 }
 
 Place.statics.findGridPlaces = function (pageIndex, pageSize, searchTerm, callback) {
-	//TODO FIX
-	return null
+	//TODO Le(s) SearchTerm(s)
+	return this.find({}, [], {skip: (pageIndex -1)*pageSize, limit: pageSize}, callback);
 }
 
 Place.statics.countUnvalidated = function (callback) {
