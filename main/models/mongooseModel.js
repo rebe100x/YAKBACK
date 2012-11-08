982823ec153f409e1b8167a9644288cb4b685d29
Camille <pic.camille@gmail.com>
2012-11-08 15:48:15 +0100

    search with wild card on place title

--- main/models/mongooseModel.js
+++ main/models/mongooseModel.js
@@ -323,7 +323,10 @@ Place.statics.findAll = function (callback) {
 
 Place.statics.findGridPlaces = function (pageIndex, pageSize, searchTerm, callback) {
 	//TODO Le(s) SearchTerm(s)
-	return this.find({}, 'title content', {skip: (pageIndex -1)*pageSize, limit: pageSize}, callback);
+	var res;
+	var search = new RegExp(searchTerm, 'i');
+	return this.find({title : search}, 'title content', {skip: (pageIndex -1)*pageSize, limit: pageSize}, callback);
+
 }
 
 Place.statics.countUnvalidated = function (callback) {
